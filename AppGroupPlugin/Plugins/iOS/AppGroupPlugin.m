b1fa048e98c09519995b42a07c5a90c34c528f32
Yuosif Hanna <41512322+yuosifhanna95@users.noreply.github.com>
2025-01-08 15:14:31 +0200

    Add files via upload

--- AppGroupPlugin/Plugins/iOS/AppGroupPlugin.m
+++ AppGroupPlugin/Plugins/iOS/AppGroupPlugin.m
@@ -26,34 +26,3 @@ const char* _GetAppGroupPath(const char* groupIdentifierCString) {
   NSString* appGroupPath = _GetAppGroupPathInternal(groupIdentifier);
   return MakeCStringCopy([appGroupPath UTF8String]);
 }
-
-const char* _SetString(const char* filePathCString, const char* keyNameCString, const char* textContentCString) {
-    
-    NSString* filePath = [NSString stringWithUTF8String:filePathCString];
-    NSString* keyName = [NSString stringWithUTF8String:keyNameCString];
-    NSString* textContent = [NSString stringWithUTF8String:textContentCString];
-    
-    NSString *myFile=[[NSBundle mainBundle] pathForResource:@"group.com.companyYH.WheelOfCoins.on.plist" ofType:@"plist"];
-    NSArray*docDir=NSSearchPathForDirectoriesInDomains(filePath, NSUserDomainMask, YES);
-    //NSString*filePath=[docDir objectAtIndex:0];
-    NSString*plistPath=[filePath stringByAppendingPathComponent:@"group.com.companyYH.WheelOfCoins.on.plist"];
-
-    //Check plist's existance using FileManager
-    NSError*err=nil;
-    NSFileManager*fManager=[NSFileManager defaultManager];
-
-    if(![fManager fileExistsAtPath:plistPath])
-    {
-        //file doesn't exist, copy file from bundle to documents directory
-
-        NSString*bundlePath=[[NSBundle mainBundle] pathForResource:@"group.com.companyYH.WheelOfCoins.on" ofType:@"plist"];
-        [fManager copyItemAtPath:bundlePath toPath:plistPath error:&err];
-    }
-
-    //Get the dictionary from the plist's path
-    NSMutableDictionary*plistDict=[[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
-     //Manipulate the dictionary
-     [plistDict setObject:textContent forKey:keyName];
-     //Again save in doc directory.
-     [plistDict writeToFile:myFile atomically:YES];
-}
